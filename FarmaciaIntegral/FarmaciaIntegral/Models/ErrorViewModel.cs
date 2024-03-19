d80d812a46e6a05f5b9db63200773528c88e74dd
DanielPovedaRomero <106356758+DanielPovedaRomero@users.noreply.github.com>
2024-03-18 21:13:07 -0600

    Se agrega el commit inicial

---
+++ FarmaciaIntegral/FarmaciaIntegral/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FarmaciaIntegral.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
