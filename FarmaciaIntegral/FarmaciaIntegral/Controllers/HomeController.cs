c60ab451000a4f0efff93e32a4a921237d3bf3ec
danielpoveda <danielpovedaromero@gmail.com>
2024-03-18 23:25:40 -0600

    Se agrega el singleton

--- FarmaciaIntegral/FarmaciaIntegral/Controllers/HomeController.cs
+++ FarmaciaIntegral/FarmaciaIntegral/Controllers/HomeController.cs
@@ -1,32 +1,91 @@
 ﻿using FarmaciaIntegral.Models;
 using Microsoft.AspNetCore.Mvc;
-using System.Diagnostics;
 
-namespace FarmaciaIntegral.Controllers
+public class HomeController : Controller
 {
-    public class HomeController : Controller
+    private static readonly object lockObject = new object();
+    private static HomeController instance;
+
+    public static HomeController ObtenerInstancia()
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        if (instance == null)
         {
-            _logger = logger;
+            lock (lockObject)
+            {
+                if (instance == null)
+                {
+                    instance = new HomeController();
+                }
+            }
         }
+        return instance;
+    }
 
-        public IActionResult Index()
-        {
-            return View();
-        }
+    public IActionResult Index()
+    {
+        var productos = ObtenerInstancia().ObtenerProductos();
+        return View(model: productos);
+    }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
+    public IActionResult Privacy()
+    {
+        return View();
+    }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
+    public List<Productos> ObtenerProductos()
+    {
+        List<Productos> productos = new List<Productos>();
+
+        productos.Add(new Productos
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+            Codigo = "001",
+            Nombre = "Producto 1",
+            Descripcion = "Descripción del Producto 1",
+            Calidad = 5,
+            Especificaciones = "Especificaciones del Producto 1",
+            FechaDeVencimiento = DateTime.Now.AddDays(30)
+        });
+
+        productos.Add(new Productos
+        {
+            Codigo = "002",
+            Nombre = "Producto 2",
+            Descripcion = "Descripción del Producto 2",
+            Calidad = 4,
+            Especificaciones = "Especificaciones del Producto 2",
+            FechaDeVencimiento = DateTime.Now.AddDays(45)
+        });
+
+        productos.Add(new Productos
+        {
+            Codigo = "003",
+            Nombre = "Producto 3",
+            Descripcion = "Descripción del Producto 3",
+            Calidad = 3,
+            Especificaciones = "Especificaciones del Producto 3",
+            FechaDeVencimiento = DateTime.Now.AddDays(60)
+        });
+
+        productos.Add(new Productos
+        {
+            Codigo = "004",
+            Nombre = "Producto 4",
+            Descripcion = "Descripción del Producto 4",
+            Calidad = 2,
+            Especificaciones = "Especificaciones del Producto 4",
+            FechaDeVencimiento = DateTime.Now.AddDays(75)
+        });
+
+        productos.Add(new Productos
+        {
+            Codigo = "005",
+            Nombre = "Producto 5",
+            Descripcion = "Descripción del Producto 5",
+            Calidad = 1,
+            Especificaciones = "Especificaciones del Producto 5",
+            FechaDeVencimiento = DateTime.Now.AddDays(90)
+        });
+
+        return productos;
     }
 }
---
+++ FarmaciaIntegral/FarmaciaIntegral/Models/Productos.cs
@@ -0,0 +1,12 @@
+﻿namespace FarmaciaIntegral.Models
+{
+    public class Productos
+    {
+        public string Codigo { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public int Calidad { get; set; }
+        public string Especificaciones { get; set; }
+        public DateTime FechaDeVencimiento { get; set; }
+    }
+}
